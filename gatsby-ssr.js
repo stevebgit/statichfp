03c163ebd90d1816e76ab8329b665cdc55fcaa48
Steve Bailey <steveb428@gmail.com>
2020-01-19 14:52:58 -0500

    testing js include 2

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -34,7 +34,7 @@ exports.onRenderBody = ({
 		<script
 			key="3"
 			type="text/javascript"
-			src="/article-card1.js"
+			src="/article-card.js"
 			//src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"
 		/>,
 	])
